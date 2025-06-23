d084f9d235f17d043ceee13ceef6ef7d38a8828c
Ahmed <ahmedbwire17@gmail.com>
2025-06-23 11:28:09 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
-
+# ========== /plants ==========
 class Plants(Resource):
 
     def get(self):
@@ -37,19 +37,43 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
 
+# ========== /plants/<int:id> ==========
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+        return make_response(jsonify(plant.to_dict()), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        data = request.get_json()
+        for attr in data:
+            if hasattr(plant, attr):
+                setattr(plant, attr, data[attr])
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
+# ========== Run App ==========
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
